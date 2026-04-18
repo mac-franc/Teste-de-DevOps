cb5c32ac4858fde817b3aef381c8c06dc6c7b2ae
mac-franc <maclald.gomes@pucpr.edu.br>
2026-04-17 21:48:52 -0300

    Update getName.py

--- getName.py
+++ getName.py
@@ -2,5 +2,10 @@ def get_full_name(first_name, last_name):
     full_name = first_name.title() + " " + last_name.title()
     return full_name
 
+def get_age(age):
+    age = age.title() + " "
+    return age
+
 
 print(get_full_name("john", "doe"))
+print(get_age("22"))
