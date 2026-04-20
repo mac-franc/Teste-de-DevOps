50c52934bc49e599d27e0bfb35030075c4b93662
mac-franc <maclald.gomes@pucpr.edu.br>
2026-04-20 17:45:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+"""Programa para formatar nome e idade."""
+
+
 def get_full_name(first_name, last_name):
-    full_name = f"{first_name.title()} {last_name.title()}"
-    return full_name
+    """Retorna o nome completo formatado."""
+    return f"{first_name.title()} {last_name.title()}"
 
 
 def get_age(age):
+    """Retorna a idade em formato texto."""
     return str(age)
 
 
